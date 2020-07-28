97c64c253cd05cec5dcaab366c11056d75726c3d
Yann Klein <yann.klein@me.com>
2020-07-28 22:42:06 +0900

    basic lecture set from 394

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// //////////////////////
+// Rehearsal
+// //////////////////////
+
+// 1.Select the button
+
+// 2. Listen to a click
+
+// 3. Change the DOM (add disabled class and change innerText to loading)
+
+
+
+// //////////////////////
+// HTTP GET request
+// //////////////////////
+
+// 1. Select elements
+
+// 2. Listen to a click
+
+// 2.5. Fetch the API
+
+// 3. Change the DOM
+
+
+
+// //////////////////////
+// HTTP POST request
+// //////////////////////
